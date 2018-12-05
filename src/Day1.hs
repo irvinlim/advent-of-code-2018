c02a413021892c603d5e45310b3e06f339edd6ad
Irvin Lim <irvinlimweiquan@gmail.com>
2018-12-06 00:14:40 +0800

    Complete Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,17 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Day1 where
 
 import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Read
 
 run :: T.Text -> T.Text
 run input =
-  let lines = T.splitOn "\n" input
-   in error "TBI"
+  let lines = T.lines input
+      result = sumFreqs $ map T.unpack lines
+   in T.pack (show result)
+
+sumFreqs :: [String] -> Int
+sumFreqs freqs =
+  let f x
+        | head x == '+' = tail x
+        | otherwise = x
+   in sum (map (read . f :: String -> Int) freqs)
--- test/Day1Test.hs
+++ test/Day1Test.hs
@@ -4,4 +4,23 @@ import Control.Exception
 import Test.HUnit
 import TestUtils
 
-tests = TestList []
+import Day1
+
+tests =
+  TestList
+    [ TestCase $
+      assertEqual "Empty array should return 0" 0 $ sumFreqs ([] :: [String])
+    , TestCase $
+      assertEqual "Positive number should return the number" 1 $ sumFreqs ["+1"]
+    , TestCase $
+      assertEqual "Negative number should return the number" (-1) $
+      sumFreqs ["-1"]
+    , TestCase $
+      assertEqual "Positive numbers should sum correctly" 3 $
+      sumFreqs ["+1", "+2"]
+    , TestCase $
+      assertEqual "Negative numbers should sum correctly" (-3) $
+      sumFreqs ["-1", "-2"]
+    , TestCase $
+      assertEqual "Mix of integers" 27907 $ sumFreqs ["-132", "+19827", "+8212"]
+    ]
