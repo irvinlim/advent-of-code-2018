eeb9e4de138931ad9a612808745a08c37d307f97
Irvin Lim <irvinlimweiquan@gmail.com>
2018-12-08 01:22:58 +0800

    Fix tests by using solveChallenge

--- test/Day1Test.hs
+++ test/Day1Test.hs
@@ -7,44 +7,36 @@
 module Day1Test where
 
 import Control.Exception
+import Data.List
 import Test.HUnit
 import TestUtils
 
-import Day1
+import Challenges.Day1
+
+makeTest' chall (name, input, expected) =
+  makeTest chall (name, intercalate "\n" input, expected)
+
+level1TestCases =
+  [ ("Empty array should return 0", [] :: [String], 0)
+  , ("Positive number should return the number", ["+1"], 1)
+  , ("Negative number should return the number", ["-1"], -1)
+  , ("Positive numbers should sum correctly", ["+1", "+2"], 3)
+  , ("Negative numbers should sum correctly", ["-1", "-2"], -3)
+  , ("Mix of integers", ["-132", "+19827", "+8212"], 27907)
+  ]
 
 level1Tests =
-  TestLabel "Level 1" $
-  TestList
-    [ TestCase $
-      assertEqual "Empty array should return 0" 0 $ level1 ([] :: [String])
-    , TestCase $
-      assertEqual "Positive number should return the number" 1 $ level1 ["+1"]
-    , TestCase $
-      assertEqual "Negative number should return the number" (-1) $
-      level1 ["-1"]
-    , TestCase $
-      assertEqual "Positive numbers should sum correctly" 3 $
-      level1 ["+1", "+2"]
-    , TestCase $
-      assertEqual "Negative numbers should sum correctly" (-3) $
-      level1 ["-1", "-2"]
-    , TestCase $
-      assertEqual "Mix of integers" 27907 $ level1 ["-132", "+19827", "+8212"]
-    ]
+  TestLabel "Level 1" $ TestList (map (makeTest' level1) level1TestCases)
+
+level2TestCases =
+  [ ("Given test case 1", ["+1", "-2", "+3", "+1"], 2)
+  , ("Given test case 2", ["+1", "-1"], 0)
+  , ("Given test case 3", ["+3", "+3", "+4", "-2", "-4"], 10)
+  , ("Given test case 4", ["-6", "+3", "+8", "+5", "-6"], 5)
+  , ("Given test case 5", ["+7", "+7", "-2", "-7", "-4"], 14)
+  ]
 
 level2Tests =
-  TestLabel "Level 2" $
-  TestList
-    [ TestLabel "Given test case 1" $
-      TestCase $ assertEqual "" 2 $ level2 ["+1", "-2", "+3", "+1"]
-    , TestLabel "Given test case 2" $
-      TestCase $ assertEqual "" 0 $ level2 ["+1", "-1"]
-    , TestLabel "Given test case 3" $
-      TestCase $ assertEqual "" 10 $ level2 ["+3", "+3", "+4", "-2", "-4"]
-    , TestLabel "Given test case 4" $
-      TestCase $ assertEqual "" 5 $ level2 ["-6", "+3", "+8", "+5", "-6"]
-    , TestLabel "Given test case 5" $
-      TestCase $ assertEqual "" 14 $ level2 ["+7", "+7", "-2", "-7", "-4"]
-    ]
+  TestLabel "Level 2" $ TestList (map (makeTest' level2) level2TestCases)
 
 tests = TestLabel "Day 1" $ TestList [level1Tests, level2Tests]
--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -8,8 +8,12 @@ module TestUtils where
 
 import Control.Exception
 import Control.Monad
+import qualified Data.Text.Lazy as T
 import GHC.Natural
 import Test.HUnit
+
+import ChallengeMux
+import Types
 
 -- See https://stackoverflow.com/a/6147930/2037090
 assertException :: (Exception e, Eq e) => e -> IO a -> IO ()
@@ -19,3 +23,10 @@ assertException ex action =
     assertFailure $ "Expected exception: " ++ show ex
   where
     isWanted = guard . (== ex)
+
+makeTest :: Show a => Challenge -> (String, String, a) -> Test
+makeTest chall (name, input, expected) =
+  let exp = show expected
+      inp = T.pack input
+      res = solveChallenge chall inp
+   in TestCase $ assertEqual name exp res
