57027fba4e1d3a141469c25ace5631a4ca292fbc
Irvin Lim <irvinlimweiquan@gmail.com>
2018-12-09 01:19:32 +0800

    Extract foldGrid for Day 03

--- src/Challenges/Day03.hs
+++ src/Challenges/Day03.hs
@@ -4,16 +4,13 @@
  - This software is released under the MIT License.
  - https://opensource.org/licenses/MIT
 -}
-module Challenges.Day03
-  ( level1
-  ) where
+module Challenges.Day03 where
 
 import qualified Data.Map as M
 import qualified Data.Text.Lazy as T
-import Data.Text.Lazy.Read
-import Text.Parsec
-import Text.ParserCombinators.Parsec.Combinator
-import Text.Printf
+import Text.Parsec (char, parse, space)
+import Text.ParserCombinators.Parsec.Combinator (sepEndBy1)
+import Text.Printf (printf)
 
 import Types
 import Utils.Parsing
@@ -37,18 +34,19 @@ level1 =
     , level = 1
     , sParse = maybeParse (sepEndBy1 claimParser eol)
     , sSolve =
-        let fill m pt =
-              if pt `M.member` m
-                then M.adjust (+ 1) pt m
-                else M.insert pt 1 m
-            aux m Claim {origin = (ox, oy), size = (sx, sy)} =
-              foldl
-                fill
-                m
-                [(x, y) | x <- [ox .. ox + sx - 1], y <- [oy .. oy + sy - 1]]
-            filled = foldl aux M.empty
+        let filled = foldl (foldGrid fill) M.empty
          in M.size . M.filter (> 1) . filled
     }
+
+foldGrid :: (b -> (Int, Int) -> b) -> b -> Claim -> b
+foldGrid f initial Claim {origin = (ox, oy), size = (sx, sy)} =
+  foldl f initial [(x, y) | x <- [ox .. ox + sx - 1], y <- [oy .. oy + sy - 1]]
+
+fill :: M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
+fill m pt =
+  if pt `M.member` m
+    then M.adjust (+ 1) pt m
+    else M.insert pt 1 m
 
 claimParser :: SParsec Claim
 claimParser = do
